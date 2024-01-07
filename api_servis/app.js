ed8171ddb58717516bbf32f88914b7e395001a31
mehmedalijaK <rg.mehmedalija@gmail.com>
2024-01-07 04:09:02 +0100

    api_auth

--- api_servis/app.js
+++ api_servis/app.js
@@ -2,11 +2,27 @@ const express = require('express')
 const { sequelize, Knjiga, Kategorija, KnjigaPisac, Pisac, StavkaNarudzbine, Narudzbina } = require("./models");
 const app = express()
 const cors = require("cors")
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const route = express.Router()
 
 const corsOptions = {
     origin: ['http://localhost:8000', 'http://127.0.0.1:8000', 'http://localhost:8081']
-  };
-  
+};
+
+function authToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.status(401).json({ msg: err });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+     if (err) return res.status(403).json({ msg: err });
+     req.user = user;
+     next();
+  });
+}
+
+route.use(authToken);
+
 app.use(cors(corsOptions));
 
 app.get('/', async (req, res) => {
