02d2d3559e839b0f4090dfe7ec5304c8fd5c0527
mehmedalijaK <rg.mehmedalija@gmail.com>
2024-01-07 18:37:29 +0100

    update HomeView

--- app_vue/src/App.vue
+++ app_vue/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/create-order">Create order</router-link>
-      <b-nav-item v-if="!token" to="/register">Register</b-nav-item>
-      <b-nav-item v-if="!token" to="/login">Log In</b-nav-item>
-      <b-nav-item v-else @click="logout()">Log Out</b-nav-item>
+      <b-navbar toggleable="lg" type="light" variant="light">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/" style="color: blue;">Home</b-nav-item>
+            <b-nav-item v-if="token" to="/create-order" style="color: green;">Create order</b-nav-item>
+            <b-nav-item v-if="!token" to="/register" style="color: orange;">Register</b-nav-item>
+            <b-nav-item v-if="!token" to="/login" style="color: red;">Log In</b-nav-item>
+            <b-nav-item v-else @click="logout()" style="color: purple;">Log Out</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </nav>
     <router-view/>
   </div>
--- app_vue/src/views/HomeView.vue
+++ app_vue/src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Project for RAF</h2>
+    <p>Mehmedalija Karisik RN4/21</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
